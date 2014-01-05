190e69586f9a5cf146461642d105e56f7f71b908
htyuan <htyuan80@gmail.com>
2014-01-05 23:01:35 +0800

    2-6INIT

---
+++ 2/2-6/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  main.c for exercise
+ *
+ *        Version:  1.0
+ *        Created:  2013年12月26日 14时37分13秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  htyuan (htyuan), htyuan80@gmail.com
+ *        Company:  aibang
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+#include"show_bytes.h"
+
+int main() {
+	int int_val = 0x00359141;
+	float float_val = 0x4A564504;
+}
