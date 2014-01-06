3cd205f04e28bdcd4fe34ed57d3e5e819e2a0589
htyuan <htyuan80@gmail.com>
2014-01-06 21:41:46 +0800

    2-6？怎么计算float类型的二进制呢(直接运行发现预期与实际不一致)

--- 2/2-6/main.c
+++ 2/2-6/main.c
@@ -19,7 +19,73 @@
 #include<stdio.h>
 #include"show_bytes.h"
 
+char *  getBinaryShow(int val, char *);
+
 int main() {
 	int int_val = 0x00359141;
 	float float_val = 0x4A564504;
+	
+	printf("0x00359141 expcted binary desc: 1101011001000101000001\n");
+	char bytes[128];
+	printf("0x00359141 actual  binary desc: %s \n", getBinaryShow(int_val, bytes));
+	printf("0x4A564504 expcted binary desc: 1001010010101100100010100000100\n");
+	printf("0x4A564504 actual  binary desc: %s \n", getBinaryShow(float_val, bytes));
+	printf("%lf = ", float_val);
 }
+
+char *  getBinaryShow(int val, char * bytes) {
+	char f[2] = {'0','1'};
+
+	// 获取除以2的所有余数
+	int tmp = val;
+	int index = 0;
+	do{
+		bytes[index] = f[tmp % 2];
+	    tmp = tmp / 2;
+		index++;	
+	} while(tmp != 0);
+
+	bytes[index] = 0;
+
+	
+	//exchange 
+	int i;
+	for (i = 0;i < index / 2;i++) {
+		char t = bytes[i];
+		bytes[i] = bytes[index - i -1];
+		bytes[index - i - 1] = t;
+	}
+
+	return bytes;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
